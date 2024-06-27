c6af7afad3b2750f56ef1c671644074581dc5d31
chocho77 <chavmom@gmail.com>
2024-06-27 14:06:49 +0300

    On branch master  Initial commit  Changes to be committed: 	new file:   lecture01/ExampleOne.java

---
+++ lecture01/ExampleOne.java
@@ -0,0 +1,8 @@
+package lecture01;
+
+public class ExampleOne{
+    public static void main(String[] args){
+        System.out.println("Hello, Java!");
+        System.out.println("This is a java program!");
+    }
+}
